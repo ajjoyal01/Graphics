fd63f901f8f0d9975dc29a3251a229629b1e6b13
ajjoyal01 <ajjoyal01@gmail.com>
2014-10-20 11:12:32 -0400

    Typed out Texture stuff (shell code)

---
+++ Project3/Project3/Texture.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <GL/glew.h>
+#include <GL/GL.h>
+#include <GL/freeglut.h>
+#include <iostream>
+#include <cstdlib>
+
+#include "vgl.h"
+
+class Texture
+{
+
+public:
+	Texture();
+	~Texture();
+
+};
